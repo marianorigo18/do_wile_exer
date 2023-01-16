d50ec40b3a965bc3176076614ebd11fdfa386c6c
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-15 23:59:33 -0300

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let valorHp;
 let valorPotencia;
+let valorTension;
 
 let option = prompt('Elige una opcion: \n1- Corriente nominal. \n2- Potencia. \n3- Amperaje. \nPresione X para finalizar.');
 
@@ -45,9 +46,36 @@ while(option != 'X' && option != 'x'){
                     alert('solo numeros porfavor')
                 }
             }while(!isNaN(valorPotencia) == false)
-            alert(valorPotencia);
-     
 
+            do{
+                valorTension = prompt('Elegí la Tension: \n1 - 12v. \n2 - 24v. \n3 - 110v. \n4 - 220v. \n5 - 380v.')
+                if(!isNaN(valorTension) == false){
+                    alert('solo numeros porfavor')
+                }
+                if(valorTension > 5){
+                    alert('No mayor a 5')
+                }else{
+                    calculadoraPotencia(Number(valorPotencia), Number(valorTension))
+                }
+            }while(Number(valorTension) > 5 || !isNaN(valorTension) == false)
+     
+            function calculadoraPotencia(potencia, tension){
+                if(tension == 1){
+                    alert(`el resultado de ${potencia}w dividido 12v es igual a = ${potencia / 12}A`)
+                }
+                if(tension == 2){
+                    alert(`el resultado de ${potencia}w dividido 24v es igual a = ${potencia / 24}A`)
+                }
+                if(tension == 3){
+                    alert(`el resultado de ${potencia}w dividido 110v es igual a = ${potencia / 110}A`)
+                }
+                if(tension == 4){
+                    alert(`el resultado de ${potencia}w dividido 220v es igual a = ${potencia / 220}A`)
+                }
+                if(tension == 5){
+                    alert(`el resultado de ${potencia}w dividido 380v es igual a = ${potencia / 380}A`)
+                }
+            }
 
             break;
         case '3':
